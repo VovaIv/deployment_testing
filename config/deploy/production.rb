8e1ed9655e23a205c91965eafc780503f1c2d278
Volodymyr Horodchuk <vovan_tl@ukr.net>
2026-01-07 14:54:04 -0600

    tweak

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,4 +1,9 @@
 server "44.204.219.122",
   user: "ubuntu",
   roles: %w{app db web},
-  primary: true
+  primary: true,
+  ssh_options: {
+    keys: ["~/Downloads/vovan.pem"],
+    forward_agent: false,
+    auth_methods: %w(publickey)
+  }
