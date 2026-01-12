a31ec2d8087755a3e9d0265227eff5458632f37a
Volodymyr Horodchuk <vovan_tl@ukr.net>
2026-01-12 11:30:00 -0600

    change deploy config

--- config/deploy.rb
+++ config/deploy.rb
@@ -3,11 +3,14 @@ lock "~> 3.20.0"
 set :application, "deployment_testing"
 set :repo_url, "git@github.com:VovaIv/deployment_testing.git"
 
+# Deploy directory
 set :deploy_to, "/home/ubuntu/deployment_testing"
 
+# Ruby version
 set :rbenv_type, :user
 set :rbenv_ruby, "3.2.1"
 
+# Shared files & directories (persist across deploys)
 set :linked_files, fetch(:linked_files, []).push(
   "config/database.yml",
   "config/master.key"
@@ -17,15 +20,26 @@ set :linked_dirs, fetch(:linked_dirs, []).push(
   "log",
   "tmp/pids",
   "tmp/sockets",
+  "storage",
   "public/system",
-  "storage"
+  "public/assets" # important for precompiled assets
 )
 
+# Keep only the last 5 releases
 set :keep_releases, 5
 
-set :puma_systemctl_user, :system  # This ensures it runs as system service
-set :puma_service_unit_name, 'puma'
-
+# Branch to deploy
 set :branch, "main"
 
+# Puma config
+set :puma_systemctl_user, :system
+set :puma_service_unit_name, 'puma'
+
+# Bundler
 set :bundle_jobs, 1
+
+# Rails master key
+set :rails_master_key, ENV['RAILS_MASTER_KEY']
+
+# Assets roles
+set :assets_roles, [:web, :app]
